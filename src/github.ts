36b91d73860f324063c45945e3eac204eb2b8ec7
seed-of-apricot <seed.of.apricot@dev.chronolapselibrary.net>
2020-03-30 00:18:41 -0700

    testing6

--- src/github.ts
+++ src/github.ts
@@ -2,12 +2,17 @@ import * as github from '@actions/github';
 import * as core from '@actions/core';
 
 const getIssues = async () => {
-  const token = core.getInput('token');
-  console.log('repository');
+  try {
+    const token = core.getInput('token');
+    console.log('repository');
 
-  const repository = github.context.repo;
+    const repository = github.context.repo;
 
-  console.log(repository);
+    console.log(repository);
+  } catch (error) {
+    console.log(error);
+    core.setFailed(error);
+  }
 };
 
 export default getIssues;
