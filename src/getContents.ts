27c6b250ecca92c45d6b51e20c37fe0b4cdc4f8e
seed-of-apricot <seed.of.apricot@dev.chronolapselibrary.net>
2020-04-06 20:40:06 -0700

    1.0.2 testing7

--- src/getContents.ts
+++ src/getContents.ts
@@ -8,7 +8,9 @@ const getContents = async () => {
     const labels = core.getInput('labels');
     const octokit = new github.GitHub(token);
 
+    console.log('1');
     console.log('GitHub client has been initialized.');
+    console.log('2');
 
     const repository = github.context.repo;
 
@@ -19,6 +21,7 @@ const getContents = async () => {
     });
     const readme = readFile('../README.md', (data: any) => {
       console.log(data);
+      console.log('3');
     });
     const arrayOfFiles = readdirSync('../');
     console.log(arrayOfFiles);
